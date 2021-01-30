41950b480ef15c6e7aa488b3739e962b5553353f
masaakiwaki <71519528+masaakiwaki@users.noreply.github.com>
2021-01-30 20:12:53 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
